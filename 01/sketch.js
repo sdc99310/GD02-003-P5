ad3d5e6c9a08b8e880afafae54e9c61571b1134f
sdc99310 <sdc99310@gmail.com>
2021-03-12 02:32:12 -0500

    01 drawing processing

--- 01/sketch.js
+++ 01/sketch.js
@@ -1,7 +1,40 @@
+var font; //creat font
+
+function preload(){ //loading page
+  font =  loadFont('font.otf'); //load the font file
+}
+
+
 function setup() { //draw once
   createCanvas(1000, 1000); //width & hight in pixels
   background(0); // color 0(black) to 100(white)
+  textFont(font); // call textfont
+  textSize(500); //size px
+  fill(300); //text color //? when the color value is 100, it's grey. guess background color effected?
+  text('W', 200, 700); //contents of text, x, y location
+
+  var points = font.textToPoints('W', 200, 700); //create x,y locations arrays
+  console.log(points); //call the varible
+
+  // for (var i = 0; i <points.length; i++){
+  //   var pt = points[i];
+  //   storke(0,255,0);
+  //   strokeWeight(4);
+  //   point(pt.x, pt.y);
+  // }
 }
 
-function draw() { //draw mutiple times
-}
+// function draw() { //draw mutiple times
+// }
+
+
+
+
+
+
+
+
+
+
+/////otf font sources
+/////https://www.fontsquirrel.com/fonts/list/popular
