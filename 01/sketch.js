ef4b0db26dcc03497baa822e39b235626fbb20bc
sdc99310 <sdc99310@gmail.com>
2021-03-12 02:47:08 -0500

    Update sketch.js

--- 01/sketch.js
+++ 01/sketch.js
@@ -8,20 +8,22 @@ function preload(){ //loading page
 function setup() { //draw once
   createCanvas(1000, 1000); //width & hight in pixels
   background(0); // color 0(black) to 100(white)
-  textFont(font); // call textfont
-  textSize(500); //size px
-  fill(300); //text color //? when the color value is 100, it's grey. guess background color effected?
-  text('W', 200, 700); //contents of text, x, y location
+  // textFont(font); // call textfont
+  // textSize(500); //size px
+  // fill(); //text color //? when the color value is 100, it's grey. guess background color effected?
+  // text('W', 200, 700); //contents of text, x, y location
 
-  var points = font.textToPoints('W', 200, 700); //create x,y locations arrays
+  //doesn't need to draw the text anymore!
+
+  var points = font.textToPoints('W', 200, 700, 500); //create x,y locations arrays //contents, x, y, size
   console.log(points); //call the varible
 
-  // for (var i = 0; i <points.length; i++){
-  //   var pt = points[i];
-  //   storke(0,255,0);
-  //   strokeWeight(4);
-  //   point(pt.x, pt.y);
-  // }
+  for (var i = 0; i <points.length; i++){ ////creat all the dots following the outline of the letters
+    var pt = points[i];
+    stroke(300); //color
+    strokeWeight(9);
+    point(pt.x, pt.y);
+  } // set the for loof to make dot's of outline of the text
 }
 
 // function draw() { //draw mutiple times
@@ -35,6 +37,6 @@ function setup() { //draw once
 
 
 
-
-/////otf font sources
-/////https://www.fontsquirrel.com/fonts/list/popular
+//////sources, references, work cited
+//////https://youtu.be/4hA7G3gup-4 / making multiple dots following the outline of the text using the for loop
+/////https://www.fontsquirrel.com/fonts/list/popular /otf font sources
