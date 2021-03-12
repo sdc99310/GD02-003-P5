e5471926c884826606ad1c90e8530d27b0ec74b4
sdc99310 <sdc99310@gmail.com>
2021-03-12 07:43:43 -0500

    sketch03

--- 03/sketch.js
+++ 03/sketch.js
@@ -1,9 +1,16 @@
 function setup() {
   createCanvas(1000, 1000);
+  background(300);
+
 }
 
-function draw() {
-  background(0);
+function draw(){
+  // if(mousePressed){
+  //   background(300);
+  // }
+  stroke(0);
+  fill(300);
+  rect(mouseX, mouseY, 150, 10);
 }
 
 
@@ -16,8 +23,6 @@ function draw() {
 
 
 
-
-
-// function mousePressed() {
-//   saveCanvas("p5-sketche-01","jpg")
-// }
+function keyPressed() {
+  saveCanvas("p5-sketche-01","png")
+}
--- 04/sketch.js
+++ 04/sketch.js
@@ -1,10 +1,9 @@
 function setup() {
   createCanvas(1000, 1000);
+  background(300);
 }
 
-function draw() {
-  background(0);
-}
+
 
 
 
