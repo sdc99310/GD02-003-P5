b9d4294cf22b60f9e68e13fa293b77a740e5ae1b
sdc99310 <sdc99310@gmail.com>
2021-03-13 18:24:07 -0500

    sketch05

--- 05/sketch.js
+++ 05/sketch.js
@@ -1,10 +1,24 @@
 function setup() {
   createCanvas(1000, 1000);
+
+  noFill();
+  
+  for (i=0; i<200; i=i+2) {
+
+    rotate(PI / 360);
+    rect(800+i/3,50+i/50,50+i,50+i*10)
+
+    push();   //Start a new drawing state
+    rotate(PI / 360);
+    rect(600+i/3,50+i/50,50+i,50+i*10)
+    pop();    // Restore original state
+  }
 }
 
-function draw() {
-  background(0);
-}
+
+// function draw() {
+//   background(0);
+// } 
 
 
 
