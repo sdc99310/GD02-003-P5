f60cb7897ab7ab9146fc8dd7d1128f2285f57b8b
sdc99310 <sdc99310@gmail.com>
2021-03-14 00:33:49 -0500

    .

--- 06/sketch.js
+++ 06/sketch.js
@@ -1,32 +1,7 @@
 function setup() {
   createCanvas(1000, 1000);
-  // background(0);
+  background(0);
 
-//   noFill(0);
-//   stroke(0);  
-  
-//     for (i=0; i<500; i=i+4) {
-      
-//       stroke(300); 
-//       //shadow
-//       rect(130+i/3-30,250+i-50 ,200 ,70)
-//       rect(470+i/-3-30,250+i-50 ,100 ,170)
-//       rect(480+i/3-30,250+i-50 ,200 ,70)
-//       rect(820+i/-3-30,250+i-50 ,100 ,170)
-
-
-//       stroke(0); 
-//       //front
-//       rect(100+i/3-30,230+i-50 ,100 ,70)
-//       rect(440+i/-3-30,230+i-50 ,100 ,70)
-//       rect(450+i/3-30,230+i-50 ,100 ,70)
-//       rect(790+i/-3-30,230+i-50 ,100 ,70)
-//     }
-
-// }
-
-//color change, appears different pattern
-  background(300);
   noFill(0);
   stroke(0);  
   
@@ -34,10 +9,10 @@ function setup() {
       
       stroke(300); 
       //shadow
-      rect(130+i/3-30,250+i-50 ,100 ,70)
-      rect(470+i/-3-30,250+i-50 ,100 ,70)
-      rect(480+i/3-30,250+i-50 ,100 ,70)
-      rect(820+i/-3-30,250+i-50 ,100 ,70)
+      rect(130+i/3-30,250+i-50 ,200 ,70)
+      rect(470+i/-3-30,250+i-50 ,100 ,170)
+      rect(480+i/3-30,250+i-50 ,200 ,70)
+      rect(820+i/-3-30,250+i-50 ,100 ,170)
 
 
       stroke(0); 
@@ -49,6 +24,32 @@ function setup() {
     }
 
 }
+
+//02.color change, appears different pattern
+
+//   background(300);//
+//   noFill(0);
+//   stroke(0); 
+  
+//     for (i=0; i<500; i=i+4) {
+      
+//       stroke(200);//
+//       //shadow
+//       rect(130+i/3-10,250+i-20 ,100 ,70)
+//       rect(470+i/-3-10,250+i-20 ,100 ,70)
+//       rect(480+i/3-10,250+i-20 ,100 ,70)
+//       rect(820+i/-3-10,250+i-20 ,100 ,70)
+
+
+//       stroke(0);//
+//       //front
+//       rect(100+i/3-10,230+i-20 ,100 ,70)
+//       rect(440+i/-3-10,230+i-20 ,100 ,70)
+//       rect(450+i/3-10,230+i-20 ,100 ,70)
+//       rect(790+i/-3-10,230+i-20 ,100 ,70)
+//     }
+
+// }
 
 
 
