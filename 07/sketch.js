8f4a569fff1fa7b1127b0538ea2e373e3156df01
sdc99310 <sdc99310@gmail.com>
2021-03-14 01:55:17 -0500

    sketch07

--- 07/sketch.js
+++ 07/sketch.js
@@ -1,14 +1,26 @@
 function setup() {
   createCanvas(1000, 1000);
+  background(300);
+
+
+  for (i=0; i<500; i=i+10) {
+    noStroke();
+
+
+    fill(-300+i+100);//middle
+    ellipse (500,450+i, 100+i,100+i);
+  
+
+    fill(400-i+30);//creat gradient white to black
+    ellipse (200,150+i,250,250);
+
+
+    fill(400-i+30);
+    ellipse (800,150+i,250,250);
+
+  }
+
 }
-
-function draw() {
-  background(0);
-}
-
-
-
-
 
 
 
