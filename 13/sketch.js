bb42c264b79c84885289ad6529a1d85f23e95377
sdc99310 <sdc99310@gmail.com>
2021-03-23 15:03:53 -0400

    .

--- 13/sketch.js
+++ 13/sketch.js
@@ -2,17 +2,25 @@ function setup() {
   createCanvas(1000, 1000);
   background(300);
 
-
-  // for (i=0; i<500; i=i+10) {
-    
-
-  // }
-
 }
 
+function draw() {
+  background(255)
+  for (var i = 90; i < 500; i++) {
+    var r = 800;
+    stroke(color(random(r),random(i),random(i), 80));   
+   
+    line(300, r + 50, i + 90, i);
+    line(800,  r+50,  700, i);
+    // push();
+    // line(700, r -50, i + 90, i)
+    // pop();
 
+    // line(700, r -50, r-300, i-50)
 
-
+    // line(950, r -150, r-300, i-50)
+  }
+}
 
 // function mousePressed() {
 //   saveCanvas("p5-sketche-07","png")
