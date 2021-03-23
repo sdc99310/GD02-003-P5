ca3dd0f1402eb4d94e9110ccd918638605725615
sdc99310 <sdc99310@gmail.com>
2021-03-23 09:50:19 -0400

    .

---
+++ 20/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(1000, 1000);
+  background(300);
+
+
+  // for (i=0; i<500; i=i+10) {
+    
+
+  // }
+
+}
+
+
+
+
+
+// function mousePressed() {
+//   saveCanvas("p5-sketche-07","png")
+// }
