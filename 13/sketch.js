fcc5aae2591fd8064da28e68eb587badb7aa1385
sdc99310 <sdc99310@gmail.com>
2021-03-23 17:24:18 -0400

    .

--- 13/sketch.js
+++ 13/sketch.js
@@ -1,27 +1,77 @@
 function setup() {
   createCanvas(1000, 1000);
   background(300);
+  // angleMode(DEGREES);
 
+  // for (var i = 90; i < 500; i++) {
+  //   var r = 800;
+  //   stroke(color(random(r),random(i),random(i), 80));  
+
+  //     // line(800, i+ 50, 590, i);//block
+  //   line(300, r+ 50, i + 90, i);
+
+  //   // push();
+  //   // translate(width / 9, height / 9); 
+  //   // rotate(PI / 360.0);
+  //   // line(700, r -50, i + 90, i);
+  //   // pop();
+
+  //   push();
+  //   translate(350, 15); 
+  //   line(300, r+ 50, i + 90, i);
+  //   pop();
+
+   
+
+  //   // line(700, r -50, r-300, i-50)
+
+  //   // line(950, r -150, r-300, i-50)
+  // }
 }
 
+
+
 function draw() {
-  background(255)
-  for (var i = 90; i < 500; i++) {
+
+  for (var i = 30; i < 500; i++) {
     var r = 800;
-    stroke(color(random(r),random(i),random(i), 80));   
+    stroke(color(random(r),random(i),random(i), 80));  
+      // line(800, i+ 50, 590, i);//block
+    // line(650, r+ 50, i + 90, i);
+
+    push();
+    translate(width / 9, height / 9); 
+    rotate(PI / 360.0);
+    line(550, r -50, i + 90, i);
+    pop();
+
+    push();
+    translate(355, 15); 
+    line(300, r+ 50, i + 90, i);
+    pop();
+
+    push();
+    translate(100, 100); 
+    line(300, r+ 50, i + 90, i);
+    pop();
    
-    line(300, r + 50, i + 90, i);
-    line(800,  r+50,  700, i);
-    // push();
-    // line(700, r -50, i + 90, i)
-    // pop();
+    push();
+    translate(-145, 195); 
+    rotate(PI / 360.0);
+    line(550, r -50, i + 90, i);
+    pop();
+    
+
+   
 
     // line(700, r -50, r-300, i-50)
 
     // line(950, r -150, r-300, i-50)
-  }
+  
+}
 }
 
-// function mousePressed() {
-//   saveCanvas("p5-sketche-07","png")
-// }
+  
+function mousePressed() {
+  saveCanvas("p5-sketche-13","png")
+}
