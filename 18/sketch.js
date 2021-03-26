996fe3ca33df3c2543bc1186e9192d3127eddbd1
sdc99310 <sdc99310@gmail.com>
2021-03-26 09:18:43 -0400

    18

--- 18/sketch.js
+++ 18/sketch.js
@@ -5,7 +5,7 @@ let canvas
 
 //load the font
 function preload(){
-  font = loadFont('ChunkFive-Regular.otf')
+  font = loadFont('FFF_Tusj.ttf')
 }
 
 //loopduration (animation)
@@ -13,7 +13,7 @@ const loopDuration = 3 * 60
 
 function setup() {
   createCanvas(1000, 1000);
-  background(0);
+  background(255);
 
   graphic = createGraphics(width, height)
 //text setup
@@ -22,17 +22,42 @@ function setup() {
   graphic.blendMode(SCREEN)
 
   //01
-  graphic.textSize(800)
-  graphic.fill('rgba(255, 0, 0, 0.9)') //red blue yellow, hue
+  graphic.textSize(2000)
+  graphic.fill('rgba(101, 216, 224, 0.5)')
+  // graphic.fill('rgba(3, 104, 205, 0.5)')
+  // graphic.fill('rgba(255, 0, 0, 0.9)') //hue saturation lightness
   graphic.text('W',width/2,height/3)// x, y location
   //02
-  graphic.textSize(800)
-  graphic.fill('rgba(0, 255, 0, 0.9)') //red blue yellow, hue
-  graphic.text('W',width/1.95,height/2.95)// x, y location change
+  graphic.textSize(2000)
+  graphic.fill('rgba(255, 164, 45, 0.9)') //
+  // graphic.fill('rgba(1, 255, 0, 0.9)') //
+  graphic.text('W',width/1.95,height/2.55)// x, y location change
+
+
+  //01
+  graphic.textSize(2000)
+  graphic.fill('rgba(3, 104, 205, 1)')
+  // graphic.fill('rgba(255, 0, 0, 0.9)') //hue saturation lightness
+  graphic.text('W',width/1,height/3)// x, y location
+  //02
+  graphic.textSize(2000)
+  graphic.fill('rgba(255, 164, 45, 0.9)') //
+  // graphic.fill('rgba(1, 255, 0, 0.9)') //
+  graphic.text('W',width/3.5,height/2.55)// x, y location change
+
+
   //03
-  graphic.textSize(800)
-  graphic.fill('rgba(0, 0, 255, 0.9)') //red blue yellow, hue
-  graphic.text('W',width/2.05,height/3.05)// x, y location change
+  graphic.textSize(900)
+  graphic.fill('rgba(177, 253, 123, 1)') //
+  // graphic.fill('rgba(0, 0, 255, 0.9)') //
+  graphic.text('W',width/2,height/1.6)// x, y location change
+
+  //04
+  graphic.textSize(900)
+  graphic.stroke('rgba(177, 253, 123, 1)') //
+  // graphic.fill('rgba(0, 0, 255, 0.9)') //
+  graphic.text('W',width/2,height/8)// x, y location change
+
 }
 
 function draw() {
@@ -42,18 +67,18 @@ function draw() {
   let v = currentFrame / loopDuration 
   let u = map (v, 0, 1, 0, 2*PI)
 
-  background(0);
+  // background('rgba(0, 255, 0, 0.9)');
 
   ///creat tiles
-  const tiles = 24
+  const tiles = 80
   const tileSize = width/ tiles
 
   //looping each of the tiles
   for (let x = 0; x < tiles; x++){
     for (let y = 0; y < tiles; y++){
 
-      const distortionX = cos (u + x *0.5)*30
-      const distortionY = sin (u + y *0.5)*30
+      const distortionX = cos (u + x *300)*10
+      const distortionY = sin (u + y *0.5)*10
 
       //applying the grid into the graphic
       const sx = x * tileSize + distortionX
@@ -79,12 +104,11 @@ function draw() {
 
 
 
+
 // function mousePressed() {
 //   saveCanvas("p5-sketche-18","png")
 // }
 
 //work cited
 //reference https://www.youtube.com/watch?v=SKDhkB8g1So
-//graphic - 
-//blend mode- 
-//distortion-
+
