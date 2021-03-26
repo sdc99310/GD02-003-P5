573391c8486603e0975473ebd3977c05b42e4e76
sdc99310 <sdc99310@gmail.com>
2021-03-26 12:45:37 -0400

    20

--- 20/sketch.js
+++ 20/sketch.js
@@ -1,12 +1,46 @@
+//pre-setting 
+let pg, font, tileSize
+let rgba = ['rgba(255, 0, 0, 0.9', 
+            'rgba(0, 255, 0, 0.9)',
+             'rgba(0, 0, 255, 0.9)']
+
+let textSize = 900
+let tiles = 80
+
+//font preload
+function preload(){
+  font = loadFont('font.otf')
+}
+
 function setup() {
   createCanvas(1000, 1000);
   background(300);
 
+  pg = createGraphics(width, height)
+  pg.blendMode(SCREEN)
 
-  // for (i=0; i<500; i=i+10) {
-    
+  ///font setup
+  pg.textFont(font)
+  pg.textAlign(CENTER, CENTER)
+  pg.translate(width / 2, height / 2)
+  pg.textSize(textSize)
+  //1
+  pg.fill(rgba[0])
+  pg.text('F',2, 2)
+  //2
+  pg.fill(rgba[1])
+  pg.text('F', 0, 0)
+  //3
+  pg.fill(rgba[2])
+  pg.text('F', -2, -2)
+  //4
+  pg.fill(rgba[2])
+  pg.text('F', 2, 2)
 
-  // }
+  tileSize = width / tiles
+
+  //frameRate
+  frameRate(30)
 
 }
 
@@ -17,3 +51,6 @@ function setup() {
 // function mousePressed() {
 //   saveCanvas("p5-sketche-20","png")
 // }
+
+///work cited
+//font https://www.fontspace.com/contender-version-font-f53852
